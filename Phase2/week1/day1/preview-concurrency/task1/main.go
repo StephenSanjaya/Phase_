c304b917fcb7daaf4cfbbd47655a3ab04bad19df
StephenSanjaya <stephensanjaya5@gmail.com>
2024-03-18 09:34:47 +0700

    preview concurrency: task3

--- Phase2/week1/day1/preview-concurrency/task1/main.go
+++ Phase2/week1/day1/preview-concurrency/task1/main.go
@@ -2,30 +2,40 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"sync"
 )
 
-func PrintNumbers(wg *sync.WaitGroup) {
+func PrintNumbers(wg *sync.WaitGroup, ch chan string) {
 	defer wg.Done()
 	for i := 1; i <= 10; i++ {
-		fmt.Println(i)
+		ch <- strconv.Itoa(i)
 	}
 }
 
-func PrintLetters(wg *sync.WaitGroup) {
+func PrintLetters(wg *sync.WaitGroup, ch chan string) {
 	defer wg.Done()
 	for i := 97; i <= 106; i++ {
-		fmt.Println(string(byte(i)))
+		ch <- string(byte(i))
 	}
 }
 
 func main() {
 
+	var ch = make(chan string)
+	defer close(ch)
+
 	var wg sync.WaitGroup
 
 	wg.Add(2)
-	go PrintLetters(&wg)
-	go PrintNumbers(&wg)
+	go PrintLetters(&wg, ch)
+	go PrintNumbers(&wg, ch)
+
+	go func() {
+		for v := range ch {
+			fmt.Println(v)
+		}
+	}()
 
 	wg.Wait()
 }
